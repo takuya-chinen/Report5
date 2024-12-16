4baa08815fcb5bfaeb31a9c0ce3edc5a898b5825
Takuya Chinen <e245745@ie.u-ryukyu.ac.jp>
2024-12-16 09:45:19 +0900

    Step3: Let's refer Java API Reference

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = null;
-        try {
-            System.out.println(str.length()); // NullPointerException
-        } catch (NullPointerException e) {
-            System.out.println("例外: NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-        }
-
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
